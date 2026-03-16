4c84d110a9aab87d6149c687505bb62c3f795b07
Nikon Veremeichik <thesecondnikon@gmail.com>
2026-03-16 13:30:23 +0500

    Decided against using context-shift because it negatively affects model output

--- assistant.ps1
+++ assistant.ps1
@@ -5,11 +5,7 @@ $model = "E:\LLMS\models\gpt-oss-20b-F16.gguf"
 # Context length
 $context = 4096
 
-# Context shift enable
-$contextShift = "--context-shift"
-
 # Execute llama-server
 & "llama-server" `
     -m $model `      # model file
     -c $context `    # context length
-    $coontextShift   # shift context if model runs out of context
