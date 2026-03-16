3f0f2dbb2b07c4225f0b254d223def724590c641
Nikon Veremeichik <thesecondnikon@gmail.com>
2026-03-16 17:27:40 +0500

    Rewrote assistant.ps1 script

--- assistant.ps1
+++ assistant.ps1
@@ -1,11 +1,6 @@
-# Start the assistant model
-# Path to the model
+$llamaServer = "llama-server"
 $model = "E:\LLMS\models\gpt-oss-20b-F16.gguf"
+$context = 0
+$verbose = "--verbose-prompt"
 
-# Context length
-$context = 4096
-
-# Execute llama-server
-& "llama-server" `
-    -m $model `      # model file
-    -c $context `    # context length
+& $llamaServer -m $model -c $context $verbose
